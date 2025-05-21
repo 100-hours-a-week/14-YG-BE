68bc30d0669534c429328b3f5b04f2c1402cc379
jieunyy <ina011011@naver.com>
2025-05-21 11:38:21 +0900

    feat(Group-buy): add 'cancel groupbuy' scheduler (#32)

--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/scheduler/GroupBuyScheduler.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/scheduler/GroupBuyScheduler.java
@@ -1,5 +1,6 @@
 package com.moogsan.moongsan_backend.domain.groupbuy.scheduler;
 
+import com.moogsan.moongsan_backend.domain.groupbuy.service.GroupBuyCommandService.CancelGroupBuyParticipant;
 import com.moogsan.moongsan_backend.domain.groupbuy.service.GroupBuyCommandService.ClosePastDueGroupBuys;
 import com.moogsan.moongsan_backend.domain.groupbuy.service.GroupBuyCommandService.EndPastPickupGroupBuys;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ public class GroupBuyScheduler {
 
     private final ClosePastDueGroupBuys closePastDueGroupBuys;
     private final EndPastPickupGroupBuys endPastPickupGroupBuys;
+    private final CancelGroupBuyParticipant cancelGroupBuyParticipant;
 
     // 공구 게시글 dueDate 기반 자동 공구 마감 스케줄러(매 정각(0분)과 30분에 작동)
     @Scheduled(cron = "0 0/30 * * * *")
@@ -22,10 +24,17 @@ public class GroupBuyScheduler {
         closePastDueGroupBuys.closePastDueGroupBuys(now);
     }
 
-    // 공구 게시글 pickupDate 기반 자동 공구 종료 스케줄러 (매 정각(0분)과 30분에 작동)
-    @Scheduled(cron = "0 0/30 * * * *")
+    // 공구 게시글 pickupDate 기반 자동 공구 종료 스케줄러 (매 정각(0분)과 31분에 작동)
+    @Scheduled(cron = "0 0/31 * * * *")
     public void endPastPickupGroupBuys() {
         LocalDateTime now = LocalDateTime.now();
         endPastPickupGroupBuys.endPastPickupGroupBuys(now);
     }
+
+    // 공구 참여 자동 취소 스케줄러 (매 정각 1분10분)과 40분에 작동)
+    @Scheduled(cron = "0 1/40 * * * *")
+    public void runAutoCancellation() {
+        cancelGroupBuyParticipant.cancelUnconfirmedOrders();
+    }
+
 }
