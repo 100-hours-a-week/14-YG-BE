b792e29fffd06d8e4be292fa4a188e055f42586c
jieunyy <ina011011@naver.com>
2025-05-28 14:32:21 +0900

    feat(chatting): add basic constructure for chatting (#93)

--- src/main/java/com/moogsan/moongsan_backend/domain/chatting/entity/ChatParticipant.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/chatting/entity/ChatParticipant.java
@@ -1,48 +1,4 @@
 package com.moogsan.moongsan_backend.domain.chatting.entity;
 
-import com.moogsan.moongsan_backend.domain.user.entity.User;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Table(name = "chat_participant")
 public class ChatParticipant {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Builder.Default
-    @Column(nullable = false)
-    private int joinSeqNo = 0;
-
-    @Column(nullable = false)
-    private LocalDateTime joinedAt;
-
-    private LocalDateTime leftAt;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chat_room_id", nullable = false)
-    private ChatRoom chatRoom;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    ///  도메인 메서드
-    public boolean isActive() {
-        return leftAt == null;
-    }
-
-    public void markLeft() {
-        this.leftAt = LocalDateTime.now();
-    }
 }
