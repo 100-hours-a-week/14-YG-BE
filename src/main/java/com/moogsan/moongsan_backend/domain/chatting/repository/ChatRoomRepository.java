cbd087c2eff0c201d1ade5a8619506615b7beb5b
jieunyy <ina011011@naver.com>
2025-05-29 17:42:56 +0900

    feat(chatting): implement long-polling chat (join room, send message, fetch messages) (#93)

--- src/main/java/com/moogsan/moongsan_backend/domain/chatting/repository/ChatRoomRepository.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/chatting/repository/ChatRoomRepository.java
@@ -3,5 +3,10 @@ package com.moogsan.moongsan_backend.domain.chatting.repository;
 import com.moogsan.moongsan_backend.domain.chatting.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByGroupBuy_IdAndType(Long postId, String type);
+
+    Optional<ChatRoom> findById(Long Id);
 }
