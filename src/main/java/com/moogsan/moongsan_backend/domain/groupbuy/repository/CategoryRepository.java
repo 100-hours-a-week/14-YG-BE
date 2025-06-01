22cde094444a5388652ffc38c000388091a3f87c
jieunyy <ina011011@naver.com>
2025-06-01 15:18:37 +0900

    chore(group-buy): relocate OrderNotFoundException to domain package (#32)

--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/repository/CategoryRepository.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/repository/CategoryRepository.java
@@ -1,4 +1,7 @@
 package com.moogsan.moongsan_backend.domain.groupbuy.repository;
 
-public interface CategoryRepository {
+import com.moogsan.moongsan_backend.domain.groupbuy.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
