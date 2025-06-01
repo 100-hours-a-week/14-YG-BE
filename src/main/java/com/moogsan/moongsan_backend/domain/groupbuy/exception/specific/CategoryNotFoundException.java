2201436c0930352362c75597cac8e4cbc6a43073
jieunyy <ina011011@naver.com>
2025-06-01 15:16:51 +0900

    chore(group-buy): relocate DuplicateRequestPreventer and add Redis error logging (#32)

--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/exception/specific/CategoryNotFoundException.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/exception/specific/CategoryNotFoundException.java
@@ -1,15 +1,4 @@
 package com.moogsan.moongsan_backend.domain.groupbuy.exception.specific;
 
-import com.moogsan.moongsan_backend.domain.groupbuy.exception.base.GroupBuyException;
-import com.moogsan.moongsan_backend.domain.groupbuy.exception.code.GroupBuyErrorCode;
-
-public class CategoryNotFoundException extends GroupBuyException {
-    public CategoryNotFoundException() {
-        super(GroupBuyErrorCode.CATEGORY_NOT_FOUND, "존재하지 않는 카테고리입니다.");
-    }
-
-    public CategoryNotFoundException(String message) {
-        super(GroupBuyErrorCode.CATEGORY_NOT_FOUND, message);
-    }
+public class CategoryNotFoundException {
 }
-
--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/repository/CategoryRepository.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/repository/CategoryRepository.java
@@ -1,7 +1,4 @@
 package com.moogsan.moongsan_backend.domain.groupbuy.repository;
 
-import com.moogsan.moongsan_backend.domain.groupbuy.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository {
 }
