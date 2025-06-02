b03ca435d3c92a74f2ffe763a1874491f093f746
jieunyy <ina011011@naver.com>
2025-06-02 16:40:18 +0900

    feat(chatting): add condition to include only chat room participants where left_at is null (#93)

--- src/main/java/com/moogsan/moongsan_backend/domain/chatting/service/query/GetLatestMessages.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/chatting/service/query/GetLatestMessages.java
@@ -5,7 +5,8 @@ import com.moogsan.moongsan_backend.domain.chatting.entity.ChatMessageDocument;
 import com.moogsan.moongsan_backend.domain.chatting.entity.ChatRoom;
 import com.moogsan.moongsan_backend.domain.chatting.exception.specific.ChatRoomNotFoundException;
 import com.moogsan.moongsan_backend.domain.chatting.exception.specific.NotParticipantException;
-import com.moogsan.moongsan_backend.domain.chatting.mapper.ChatMessageMapper;
+import com.moogsan.moongsan_backend.domain.chatting.mapper.ChatMessageCommandMapper;
+import com.moogsan.moongsan_backend.domain.chatting.mapper.ChatMessageQueryMapper;
 import com.moogsan.moongsan_backend.domain.chatting.repository.ChatMessageRepository;
 import com.moogsan.moongsan_backend.domain.chatting.repository.ChatParticipantRepository;
 import com.moogsan.moongsan_backend.domain.chatting.repository.ChatRoomRepository;
@@ -28,7 +29,7 @@ public class GetLatestMessages {
     private final ChatMessageRepository chatMessageRepository;
     private final ChatParticipantRepository chatParticipantRepository;
     private final ChatRoomRepository chatRoomRepository;
-    private final ChatMessageMapper chatMessageMapper;
+    private final ChatMessageQueryMapper chatMessageQueryMapper;
 
     // 채팅방별 롱폴링 요청 큐
     private final Map<Long, List<DeferredResult<List<ChatMessageResponse>>>> listeners = new ConcurrentHashMap<>();
@@ -41,7 +42,7 @@ public class GetLatestMessages {
                 .orElseThrow(ChatRoomNotFoundException::new);
 
         // 참여자인지 조회 -> 아니면 403
-        boolean isParticipant = chatParticipantRepository.existsByChatRoom_IdAndUser_Id(chatRoomId, currentUser.getId());
+        boolean isParticipant = chatParticipantRepository.existsByChatRoom_IdAndUser_IdAndLeftAtIsNull(chatRoomId, currentUser.getId());
 
         if(!isParticipant) {
             throw new NotParticipantException("참여자만 메세지를 조회할 수 있습니다.");
@@ -71,7 +72,7 @@ public class GetLatestMessages {
         Long chatRoomId = newMessage.getChatRoomId();
         List<DeferredResult<List<ChatMessageResponse>>> results = listeners.getOrDefault(chatRoomId, new ArrayList<>());
 
-        ChatMessageResponse response = chatMessageMapper.toMessageResponse(newMessage, nickname, imageKey);
+        ChatMessageResponse response = chatMessageQueryMapper.toMessageResponse(newMessage, nickname, imageKey);
         for (DeferredResult<List<ChatMessageResponse>> r : results) {
             r.setResult(List.of(response));
         }
@@ -81,7 +82,7 @@ public class GetLatestMessages {
 
     private DeferredResult<List<ChatMessageResponse>> wrapResult(List<ChatMessageDocument> messages) {
         List<ChatMessageResponse> responses = messages.stream()
-                .map(doc -> chatMessageMapper.toMessageResponse(doc, "알수없음", null)) // 빠른 반환이라 간략화
+                .map(doc -> chatMessageQueryMapper.toMessageResponse(doc, "알수없음", null)) // 빠른 반환이라 간략화
                 .collect(Collectors.toList());
         DeferredResult<List<ChatMessageResponse>> result = new DeferredResult<>();
         result.setResult(responses);
