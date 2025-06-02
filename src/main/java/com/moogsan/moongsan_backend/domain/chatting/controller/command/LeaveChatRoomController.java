788a3dee0604a7afaccd8aa660d684afa23f4512
jieunyy <ina011011@naver.com>
2025-06-02 16:36:40 +0900

    feat(chatting): implement temporary background API for leaving participant chat room (#93)

--- src/main/java/com/moogsan/moongsan_backend/domain/chatting/controller/command/LeaveChatRoomController.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/chatting/controller/command/LeaveChatRoomController.java
@@ -1,4 +1,34 @@
 package com.moogsan.moongsan_backend.domain.chatting.controller.command;
 
+import com.moogsan.moongsan_backend.domain.WrapperResponse;
+import com.moogsan.moongsan_backend.domain.chatting.Facade.command.ChattingCommandFacade;
+import com.moogsan.moongsan_backend.domain.chatting.dto.command.response.CommandChattingReponse;
+import com.moogsan.moongsan_backend.domain.user.entity.CustomUserDetails;
+import com.moogsan.moongsan_backend.global.exception.specific.UnauthenticatedAccessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chats/participant")
 public class LeaveChatRoomController {
+
+    private final ChattingCommandFacade chattingCommandFacade;
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<WrapperResponse<CommandChattingReponse>> leaveChatRoom(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("postId") Long postId
+    ) {
+        if (userDetails == null) throw new UnauthenticatedAccessException("로그인이 필요합니다.");
+
+        chattingCommandFacade.leaveChatRoom(userDetails.getUser(), postId);
+
+        return ResponseEntity.ok(
+                WrapperResponse.<CommandChattingReponse>builder()
+                        .message("참여자 채팅방을 성공적으로 나갔습니다.")
+                        .build());
+    }
 }
--- src/main/java/com/moogsan/moongsan_backend/domain/chatting/service/command/LeaveChatRoom.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/chatting/service/command/LeaveChatRoom.java
@@ -1,4 +1,60 @@
 package com.moogsan.moongsan_backend.domain.chatting.service.command;
 
+import com.moogsan.moongsan_backend.domain.chatting.entity.ChatParticipant;
+import com.moogsan.moongsan_backend.domain.chatting.entity.ChatRoom;
+import com.moogsan.moongsan_backend.domain.chatting.exception.specific.ChatRoomNotFoundException;
+import com.moogsan.moongsan_backend.domain.chatting.exception.specific.NotParticipantException;
+import com.moogsan.moongsan_backend.domain.chatting.repository.ChatParticipantRepository;
+import com.moogsan.moongsan_backend.domain.chatting.repository.ChatRoomRepository;
+import com.moogsan.moongsan_backend.domain.groupbuy.entity.GroupBuy;
+import com.moogsan.moongsan_backend.domain.groupbuy.exception.specific.GroupBuyInvalidStateException;
+import com.moogsan.moongsan_backend.domain.groupbuy.exception.specific.GroupBuyNotFoundException;
+import com.moogsan.moongsan_backend.domain.groupbuy.repository.GroupBuyRepository;
+import com.moogsan.moongsan_backend.domain.order.entity.Order;
+import com.moogsan.moongsan_backend.domain.order.exception.specific.OrderNotFoundException;
+import com.moogsan.moongsan_backend.domain.order.repository.OrderRepository;
+import com.moogsan.moongsan_backend.domain.user.entity.User;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class LeaveChatRoom {
+
+    private final GroupBuyRepository groupBuyRepository;
+    private final OrderRepository orderRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatParticipantRepository chatParticipantRepository;
+
+    public void leaveChatRoom(User currentUser, Long groupBuyId) {
+
+        // 해당 공구의 주문 테이블에 해당 유저의 주문이 존재하는지 조회 -> 아니면 404
+        Order order = orderRepository.findByUserIdAndGroupBuyIdAndStatusNot(currentUser.getId(), groupBuyId, "CANCELED")
+                .orElseThrow(() -> new OrderNotFoundException("공구의 참여자만 나갈 수 있습니다: ORDER."));
+
+        // 해당 공구의 참여자 채팅방이 존재하는지 조회
+        ChatRoom chatRoom = chatRoomRepository
+                .findByGroupBuy_IdAndType(groupBuyId, "PARTICIPANT")
+                .orElseThrow(() -> new ChatRoomNotFoundException("존재하는 채팅방만 나갈 수 있습니다."));
+
+        // 참여자인지 조회 -> 아니면 403
+        ChatParticipant chatParticipant = chatParticipantRepository
+                .findByChatRoom_IdAndUser_IdAndLeftAtIsNull(chatRoom.getId(), currentUser.getId())
+                .orElseThrow(() -> new NotParticipantException("공구의 참여자만 나갈 수 있습니다: CHATPARTICIPANT"));
+
+        chatRoom.decrementParticipants();
+        chatRoomRepository.save(chatRoom);
+
+        chatParticipant.markLeft();
+        chatParticipantRepository.save(chatParticipant);
+
+    }
 }
