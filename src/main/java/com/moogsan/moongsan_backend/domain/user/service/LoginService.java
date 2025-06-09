9ab497125e3f097b88632046a3fcea20278c9ceb
khonetgithub <k_honet@naver.com>
2025-06-09 11:09:38 +0900

    fix(user): 로그아웃 수정 취소(기존 로직 작동함)

--- src/main/java/com/moogsan/moongsan_backend/domain/user/service/LoginService.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/user/service/LoginService.java
@@ -11,7 +11,6 @@ import com.moogsan.moongsan_backend.domain.user.repository.UserRepository;
 import com.moogsan.moongsan_backend.global.security.jwt.JwtUtil;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -56,10 +55,6 @@ public class LoginService {
                 throw new UserException(UserErrorCode.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
             }
 
-            // 탈퇴 복구 및 마지막 로그인 시간 기록
-            if (user.getDeletedAt() != null) {
-                user.setDeletedAt(null);
-            }
             user.setLastLoginAt();
 
             // JWT 토큰 발급
