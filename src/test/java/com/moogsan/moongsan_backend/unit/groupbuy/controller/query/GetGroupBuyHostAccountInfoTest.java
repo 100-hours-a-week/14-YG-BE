fe5c535fb5d5360d0dcec614496f73ac8bd58d1b
Jieun Park <107592904+jieunyy@users.noreply.github.com>
2025-06-10 21:40:24 +0900

    GroupBuy 업데이트용 조회 시 URL 추가 반환 및 MockMvc 설정 보완 (#174)
    * chore(group-buy): return url for update groupbuy (#32)
    
    * chore(group-buy): add AutoConfigureMockMvc annotation (#32)
    
    * chore(main): accept driver name via direct input instead of variable

--- src/test/java/com/moogsan/moongsan_backend/unit/groupbuy/controller/query/GetGroupBuyHostAccountInfoTest.java
+++ src/test/java/com/moogsan/moongsan_backend/unit/groupbuy/controller/query/GetGroupBuyHostAccountInfoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(GroupBuyHostAccountController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class GetGroupBuyHostAccountInfoTest {
 
     @Autowired
