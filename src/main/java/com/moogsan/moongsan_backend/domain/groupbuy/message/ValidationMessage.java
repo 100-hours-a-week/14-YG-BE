dda49f5e12fe6302e3c0fea1bbb3597b310b7df6
jieunyy <ina011011@naver.com>
2025-06-10 23:41:52 +0900

    feat(group-buy): add NotBlankIfPresent annotation and its validator (#32)

---
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/message/ValidationMessage.java
@@ -0,0 +1,4 @@
+package com.moogsan.moongsan_backend.domain.groupbuy.message;
+
+public class ValidationMessage {
+}
---
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/validator/NotBlankIfPresentValidator.java
@@ -0,0 +1,17 @@
+package com.moogsan.moongsan_backend.domain.groupbuy.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class NotBlankIfPresentValidator implements ConstraintValidator<NotBlankIfPresent, String> {
+
+    @Override
+    public void initialize(NotBlankIfPresent constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return s == null || !s.trim().isEmpty();
+    }
+}
