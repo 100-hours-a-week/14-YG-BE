5a5e9d33862c0bf23a0612c786bd72f8a7c92f6c
lunghyun <nangmannsh@gmail.com>
2025-06-17 16:21:45 +0900

    feat(cicd): 혼재된 v10 내용 수정

--- src/main/resources/db/migration/V10__rename_pickup_change_reason.sql
+++ src/main/resources/db/migration/V10__rename_pickup_change_reason.sql
@@ -1,9 +1,4 @@
--- V10__rename_modification_reason.sql
--- MySQL
+-- V10__rename_pickup_change_reason.sql
+-- Rename pickup_change_reason column to date_modification_reason
 ALTER TABLE group_buy
   RENAME COLUMN pickup_change_reason TO date_modification_reason;
-
--- H2
--- H2는 RENAME COLUMN 대신 아래 구문을 사용해야 합니다
-ALTER TABLE group_buy
-  ALTER COLUMN pickup_change_reason RENAME TO date_modification_reason;
