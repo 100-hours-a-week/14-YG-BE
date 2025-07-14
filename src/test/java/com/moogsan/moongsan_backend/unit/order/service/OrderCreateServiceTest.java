bcd3d50f9cc653a8b5ef0927cacca1f6d218f198
jieunyy <ina011011@naver.com>
2025-07-14 11:03:42 +0900

    chore(order): 코드 문법 오류 수정

--- src/test/java/com/moogsan/moongsan_backend/unit/order/service/OrderCreateServiceTest.java
+++ src/test/java/com/moogsan/moongsan_backend/unit/order/service/OrderCreateServiceTest.java
@@ -3,7 +3,7 @@ package com.moogsan.moongsan_backend.unit.order.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moogsan.moongsan_backend.adapters.kafka.producer.mapper.GroupBuyEventMapper;
 import com.moogsan.moongsan_backend.adapters.kafka.producer.publisher.KafkaEventPublisher;
-import com.moogsan.moongsan_backend.domain.chatting.participant.Facade.command.ChattingCommandFacade;
+import com.moogsan.moongsan_backend.domain.chatting.participant.facade.command.ChattingCommandFacade;
 import com.moogsan.moongsan_backend.domain.groupbuy.entity.GroupBuy;
 import com.moogsan.moongsan_backend.domain.groupbuy.policy.DueSoonPolicy;
 import com.moogsan.moongsan_backend.domain.groupbuy.repository.GroupBuyRepository;
